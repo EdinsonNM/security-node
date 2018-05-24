469d474c4a2c21468badceca22dfaa614db0bb74
Edinson Nuñez More <nmedinson@gmail.com>
2018-05-24 01:25:56 -0500

    Fix connection and token controllers

--- server/src/controllers/token.js
+++ server/src/controllers/token.js
@@ -2,20 +2,20 @@ const { Token } = require('../models');
 const uuidv1 = require("uuid/v1");
 class TokenController{
 	static get(req, res){
-		Model.findById(req.params.id)
+		Token.findById(req.params.id)
 		.then((response)=> {
 			res.json(response);
 		})
 	}
 	static getLastToken(req, res){
-		Model.findOne({_app: req.params.app, active: true})
+		Token.findOne({_app: req.params.app, active: true})
 		.then((response)=> {
 			res.json(response);
 		});
 	}
 	static post(req, res){
-		Model.where({}).update({ active: false }, () => {
-			const token = new Model({token: uuidv1(), _app: req.body._app});
+		Token.where({}).update({ active: false }, () => {
+			const token = new Token({token: uuidv1(), _app: req.body._app});
 			token.save((error, data) => {
 				if(error){
 					return res.status(500).json(error);
