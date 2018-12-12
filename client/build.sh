d77b3dedf4cf1a048f1baf2002909084d7612ed2
Edinson Nuñez More <nmedinson@gmail.com>
2018-12-12 01:03:40 -0500

    build.sh

--- client/build.sh
+++ client/build.sh
@@ -2,4 +2,4 @@
 #La ruta "/var/www/html/security-node" puede variar dependiendo de la ubicación del servidor web
 npm install
 npm run build
-cp -a ./build/. /var/www/html/security-nodess
+cp -a ./build/. /var/www/html/security-node
